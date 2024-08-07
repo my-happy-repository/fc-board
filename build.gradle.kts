d69517c3c2c59f34e68b264879634fc15dee4aeb
behappyleee <dhfhfkwhdk@naver.com>
2024-08-07 23:00:00 +0900

    feat: edit build gradle and application.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,11 @@ plugins {
     id("org.springframework.boot") version "3.1.1"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
+
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
-    kotlin("kapt") version "1.9.20"
+    // kotlin("kapt") version "1.8.22"
 }
 
 group = "com.project"
@@ -29,7 +30,9 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    implementation("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    // kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")

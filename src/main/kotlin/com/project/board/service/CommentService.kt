a1386889dda1ab558a8f1cec815715357e700fba
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-09-04 00:36:37 +0900

    feat: comment 저장 기능 추가

--- src/main/kotlin/com/project/board/service/CommentService.kt
+++ src/main/kotlin/com/project/board/service/CommentService.kt
@@ -1,14 +1,26 @@
 package com.project.board.service
 
+import com.project.board.repository.CommentRepository
+import com.project.board.repository.PostRepository
 import com.project.board.service.dto.CommentCreateRequestDto
 import org.springframework.stereotype.Service
 
 @Service
-class CommentService {
+class CommentService(
+    private val commentRepository: CommentRepository,
+    private val postRepository: PostRepository,
+) {
 
     fun createComment(
         commentCreateRequestDto: CommentCreateRequestDto,
     ): Long {
-        return 1L
+        val post = postRepository.findById(commentCreateRequestDto.postId).get()
+        val commentEntity = CommentCreateRequestDto.toComment(commentCreateRequestDto, post)
+
+        val savedComment = commentRepository.save(
+            commentEntity
+        )
+
+        return savedComment.id
     }
 }
--- src/main/kotlin/com/project/board/service/dto/CommentCreateRequestDto.kt
+++ src/main/kotlin/com/project/board/service/dto/CommentCreateRequestDto.kt
@@ -1,7 +1,20 @@
 package com.project.board.service.dto
 
+import com.project.board.domain.Comment
+import com.project.board.domain.Post
+
 data class CommentCreateRequestDto(
     val postId: Long,
     val content: String,
     val createdBy: String,
-)
+) {
+    companion object {
+        fun toComment(commentRequestDto: CommentCreateRequestDto, post: Post): Comment {
+            return Comment(
+                post = post,
+                content = commentRequestDto.content,
+                createdBy = commentRequestDto.createdBy,
+            )
+        }
+    }
+}

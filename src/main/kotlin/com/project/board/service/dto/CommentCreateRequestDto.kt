4c4ba633a3fb5620045d4300e15d78588c1f5090
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-09-03 23:53:30 +0900

    feat: comment 생성 테스트 코드 추가

--- src/main/kotlin/com/project/board/service/dto/CommentCreateRequestDto.kt
+++ src/main/kotlin/com/project/board/service/dto/CommentCreateRequestDto.kt
@@ -1,16 +1,7 @@
 package com.project.board.service.dto
 
-import com.project.board.domain.Comment
-import com.project.board.domain.Post
-
 data class CommentCreateRequestDto(
     val postId: Long,
     val content: String,
     val createdBy: String,
-)
-
-fun CommentCreateRequestDto.toEntity(post: Post) = Comment(
-    content = content,
-    createdBy = createdBy,
-    post = post
 )

9bbb10fb3bb750c1478d52d8b524a155479fd3de
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-09-18 21:59:12 +0900

    게시글 상세 조회 시 N+1 문제 헤걀 (#8)

--- src/main/kotlin/com/project/board/repository/TagRepository.kt
+++ src/main/kotlin/com/project/board/repository/TagRepository.kt
@@ -1,10 +1,36 @@
 package com.project.board.repository
 
+import com.project.board.domain.QTag.tag
 import com.project.board.domain.Tag
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TagRepository : JpaRepository<Tag, Long> {
+interface TagRepository : JpaRepository<Tag, Long>, CustomTagRepository {
     fun findByPostId(postId: Long): List<Tag>
 }
+
+interface CustomTagRepository {
+
+    fun findPageBy(pageRequest: Pageable, tagName: String): Page<Tag>
+
+}
+
+class CustomTagRepositoryImpl : CustomTagRepository, QuerydslRepositorySupport(Tag::class.java) {
+    override fun findPageBy(pageRequest: Pageable, tagName: String): Page<Tag> {
+       return from(tag)
+           // N+1 문제 해결을 위하여 join 을 직접 해 줌 !
+           .join(tag.post()).fetchJoin()
+           .where(tag.name.eq(tagName))
+           .orderBy(tag.post().createdBy.desc())
+           .offset(pageRequest.offset)
+           .limit(pageRequest.pageSize.toLong())
+           .fetchResults()
+           .let { PageImpl(it.results, pageRequest, it.total) }
+    }
+}
+

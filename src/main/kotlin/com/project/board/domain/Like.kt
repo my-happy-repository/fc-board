bf7822065a1fe4e7795999dc321f9d6f921020a0
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-09-18 22:51:31 +0900

    db 인덱스 관련 README 파일 추가 및 Tag Repository 수정 (#9)

--- src/main/kotlin/com/project/board/domain/Like.kt
+++ src/main/kotlin/com/project/board/domain/Like.kt
@@ -1,10 +1,13 @@
 package com.project.board.domain
 
+import jakarta.persistence.ConstraintMode
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
@@ -20,6 +23,7 @@ open class Like(
     open val id: Long = 0L
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))  // 조인 테이블 제약 조건을 설정 !, 제약 조건을 생성 안함
     open var post: Post = post
         protected set
 }
--- src/main/kotlin/com/project/board/domain/Tag.kt
+++ src/main/kotlin/com/project/board/domain/Tag.kt
@@ -1,10 +1,13 @@
 package com.project.board.domain
 
+import jakarta.persistence.ConstraintMode
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 
 @Entity
@@ -21,6 +24,7 @@ open class Tag(
         protected set // protected set 으로 외부 수정 불가 하도록 함
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))  // 조인 테이블 제약 조건을 설정 !, 제약 조건을 생성 안함
     open var post: Post = post
         protected set
 }
